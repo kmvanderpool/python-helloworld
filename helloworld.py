f41816d371bb59e87728e640d2461c3deb67c50c
Keith Vanderpool <keith.vanderpool@gmail.com>
2024-09-07 13:07:13 -0700

    First program

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print('Hello world')
+choice = input('Do you love lamp (Y/N): ')
+if choice == 'Y': 
+    print('Good. All should love the lamp')
+else: 
+    print('Repent now for love of the lamp is required!!!!!')
