cd5064c7e50dfae1cb76da4780b232fe0fae69ab
Keith Vanderpool <keith.vanderpool@gmail.com>
2024-09-15 18:32:44 -0700

    add multiple circles in and out

--- graphic.py
+++ graphic.py
@@ -1,15 +1,37 @@
 import turtle
-turtle.bgcolor("black")
+turtle.bgcolor("white")
 
 squary = turtle.Turtle()
-squary.speed(20)
+squary.speed(100)
+squary.pensize(1)
+
 squary.pencolor("red")
 for i in range(200):
-    squary.forward(i)
+    squary.forward(i*2)
     squary.left(91)
-
 
 squary.pencolor("blue")
 for i in range(200, 0, -1):
-    squary.back(i)
-    squary.right(91)
+    squary.forward(i*2)
+    squary.left(91)
+
+squary.pencolor("yellow")
+for i in range(200):
+    squary.forward(i*2)
+    squary.left(91)
+
+squary.pencolor("green")
+for i in range(200, 0, -1):
+    squary.forward(i*2)
+    squary.left(91)
+
+squary.pencolor("pink")
+for i in range(200):
+    squary.forward(i*2)
+    squary.left(91)
+    
+
+squary.pencolor("orange")
+for i in range(200, 0, -1):
+    squary.forward(i*2)
+    squary.left(91)
